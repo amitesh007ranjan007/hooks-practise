8a63d335fdb6ab5a0ca1c76e790dabfcd38f3427
unknown <amitesh007ranjan@gmail.com>
2021-01-07 15:36:09 +1100

    added loader UI and errorModal

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,11 +3,17 @@ import React, { useState, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
+  const [ isLoading, setIsLoading] = useState(false);
+  const [ error, setError] = useState();
 
   const ingredientsHandler = async (ingredient) => {
+
+    try {
+      setIsLoading(true)
     const response = await fetch('https://react-hooks-update-f7413-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
@@ -17,22 +23,42 @@ const Ingredients = () => {
       setUserIngredients(prevState => (
       [...prevState, {id: responseData.name, ...ingredient}]
     ))
+    setIsLoading(false)
+    } catch(error) {
+      setError('Something went wrong');
+      setIsLoading(false)
+    }
+    
   }
   const removeIngredientHandler = (id) => {
-    setUserIngredients(prevState => {
-      const prevIngredients = [...prevState]
-       const updatedIngredients = prevIngredients.filter(ing => id !== ing.id)
-       return updatedIngredients
-    })
+    setIsLoading(true)
+    fetch(`https://react-hooks-update-f7413-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
+      method: 'DELETE'
+    }).then(() => {
+      setIsLoading(false)
+      setUserIngredients(prevState => {
+        const prevIngredients = [...prevState]
+         const updatedIngredients = prevIngredients.filter(ing => id !== ing.id)
+         return updatedIngredients
+      })
+    }).catch(error => {
+      setIsLoading(false)
+      setError('Something went wrong');
 
+    })
   }
+
   const setFilteredData = useCallback(ingredientName => {
     setUserIngredients(ingredientName)
   }, [])
+
+  const resetError = () => {
+    setError(null)
+  }
   return (
     <div className="App">
-      {console.log('Ingredients rendered again')}
-      <IngredientForm onIngredientsAdd={ingredientsHandler}/>
+      {error ? <ErrorModal onClose={resetError}>{error}</ErrorModal> : null}
+      <IngredientForm onIngredientsAdd={ingredientsHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadFilteredData={setFilteredData}/>
