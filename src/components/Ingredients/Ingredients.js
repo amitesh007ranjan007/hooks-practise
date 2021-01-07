92d4445d49f53aeb1d29e800555f6702c21f5449
unknown <amitesh007ranjan@gmail.com>
2021-01-07 16:10:10 +1100

    replaced useStates with useReducer for better state management

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,64 +1,118 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
 import IngredientList from './IngredientList';
 import ErrorModal from '../UI/ErrorModal';
 
+const ingredientsReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET': 
+      return action.ingredients;
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'REMOVE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default: 
+      throw new Error('error occurred')
+  }
+}
+
+const httpReducer = (currentHttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return {
+        error: false,
+        loading: true
+      }
+    case 'RESPONSE':
+      return {
+        ...currentHttpState,
+        loading: false
+      }
+    case 'ERROR':
+      return {
+        error: true,
+        loading: false
+      }
+    case 'RESET':
+      return {
+        ...currentHttpState,
+        error: false
+      }
+    default:
+      throw new Error('Error');
+  }
+}
+
 const Ingredients = () => {
-  const [userIngredients, setUserIngredients] = useState([]);
-  const [ isLoading, setIsLoading] = useState(false);
-  const [ error, setError] = useState();
+  const [userIngredients, dispatch] = useReducer(ingredientsReducer, [])
+  const [httpState, httpDispatch] = useReducer(httpReducer, {error: false, loading: false})
 
   const ingredientsHandler = async (ingredient) => {
 
     try {
-      setIsLoading(true)
+      httpDispatch({
+        type: 'SEND'
+      })
     const response = await fetch('https://react-hooks-update-f7413-default-rtdb.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: {'Content-Type': 'application/json'}
     })
     const responseData = await response.json()
-      setUserIngredients(prevState => (
-      [...prevState, {id: responseData.name, ...ingredient}]
-    ))
-    setIsLoading(false)
+     dispatch({
+       type: 'ADD',
+       ingredient: {id: responseData.name, ...ingredient}
+     })
+     httpDispatch({
+      type: 'RESPONSE'
+    })
     } catch(error) {
-      setError('Something went wrong');
-      setIsLoading(false)
+      httpDispatch({
+        type: 'ERROR'
+      })
     }
     
   }
   const removeIngredientHandler = (id) => {
-    setIsLoading(true)
+    httpDispatch({
+      type: 'SEND'
+    })
     fetch(`https://react-hooks-update-f7413-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
       method: 'DELETE'
     }).then(() => {
-      setIsLoading(false)
-      setUserIngredients(prevState => {
-        const prevIngredients = [...prevState]
-         const updatedIngredients = prevIngredients.filter(ing => id !== ing.id)
-         return updatedIngredients
+      httpDispatch({
+        type: 'RESPONSE'
+      })
+      dispatch({
+        type: 'REMOVE',
+        id: id 
       })
     }).catch(error => {
-      setIsLoading(false)
-      setError('Something went wrong');
+      httpDispatch({
+        type: 'ERROR'
+      })
 
     })
   }
 
   const setFilteredData = useCallback(ingredientName => {
-    setUserIngredients(ingredientName)
+    dispatch({
+      type: 'SET',
+      ingredients: ingredientName
+    })
   }, [])
 
   const resetError = () => {
-    setError(null)
+    httpDispatch({
+      type: 'RESET'
+    })
   }
   return (
     <div className="App">
-      {error ? <ErrorModal onClose={resetError}>{error}</ErrorModal> : null}
-      <IngredientForm onIngredientsAdd={ingredientsHandler} loading={isLoading}/>
+      {httpState.error ? <ErrorModal onClose={resetError}>{httpState.error}</ErrorModal> : null}
+      <IngredientForm onIngredientsAdd={ingredientsHandler} loading={httpState.loading}/>
 
       <section>
         <Search onLoadFilteredData={setFilteredData}/>
